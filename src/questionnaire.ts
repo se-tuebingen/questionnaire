517ea89d3587b06cf9d3f4ef5bb360bd3d8b156e
Florian Kellner <mr.florian.kellner@web.de>
2022-03-17 10:39:10 +0100

    basic interface

---
+++ src/questionnaire.ts
@@ -0,0 +1,9 @@
+// #### QUESTIONNAIRE MODULE ####
+// This module implements the questionnaire functionality
+
+// <questionnaire> <- List of questions
+//   <question type="multiplechoice|singlechoice">
+//     <p>Question Text
+//     <answer correct="true|false">
+//       <p>Answer Text
+//       <explanation>Explanation Text

ce70f3d83f80a1837a2f5804a13e17372e2e2aab
Linus S <66415456+linususer@users.noreply.github.com>
2022-03-20 23:57:35 +0100

    added more features
    - added CollapseAll Button (hide or show all answers)
    
    - added wrapper for question and answer design
    
    - highlighting correct answers
    
    - show all answers for single-choice / only one for multiple choice
    
    - show correct answers

--- dist/questionnaire.js
+++ dist/questionnaire.js
@@ -8,29 +8,129 @@
 //       <p>Answer Text
 //       <explanation>Explanation Text
 var q_col = document.getElementsByTagName("questionnaire");
-// render every questionnaire
+// render every questionnaire in the HTML Document
 for (var i = q_col.length - 1; i >= 0; i--) {
     var questionnaire = q_col[i];
     renderQuestionaire(questionnaire);
 }
-// render questionnaire
+// render questionnaire:
+// addEventListener for "click"-Events
+// build wrapper-<div> and <img>-icons for
+// - answer
+// - question
 function renderQuestionaire(questionnaire) {
     console.log(questionnaire);
-    var a_col = questionnaire.getElementsByTagName("answer");
-    //addEventListener for every <answer>
-    for (var i = a_col.length - 1; i >= 0; i--) {
-        var answer = a_col[i];
-        answer.addEventListener("click", showExplanation);
+    renderQuestions(questionnaire);
+    renderAnswers(questionnaire);
+}
+// renderQuestions
+// for every question:
+// add <div>-wrapper + <img>-icon (done)
+// add EventListener for CollapseAll-Function
+function renderQuestions(questionnaire) {
+    var questions = questionnaire.getElementsByTagName("question");
+    for (var i = questions.length - 1; i >= 0; i--) {
+        var question = questions[i];
+        var text = question.getElementsByTagName("p")[0];
+        // build div-wrapper
+        var new_div = document.createElement("div");
+        new_div.setAttribute("class", "wrapper-question");
+        question.prepend(new_div);
+        // append text and img
+        var img = document.createElement("img");
+        img.setAttribute("src", "ressources/icons/plus-solid.svg");
+        new_div.append(text, img);
+        img.addEventListener("click", ExplanationEventHandler.bind(img, true));
     }
 }
-// get answers
+// questionnaire->DOM Manipulation
+function renderAnswers(questionnaire) {
+    var answers = questionnaire.getElementsByTagName("answer");
+    //for every answer:
+    // add <div> wrapper + <img>-icon (done)
+    // add EventListener for AnswerClickEvents (done)
+    for (var i = answers.length - 1; i >= 0; i--) {
+        var answer = answers[i];
+        // build div-wrapper
+        var new_div = document.createElement("div");
+        var text = answer.getElementsByTagName("p")[0];
+        new_div.setAttribute("class", "wrapper-answer");
+        answer.prepend(new_div);
+        //append text and img
+        var img = document.createElement("img");
+        img.setAttribute("src", "ressources/icons/circle-regular.svg");
+        new_div.append(img, text);
+        answer.addEventListener("click", checkAnswer);
+        answer.addEventListener("click", ExplanationEventHandler.bind(answer, false));
+    }
+}
+// ExplanationEventHandler
+// Handles Events for shoowing explanation text
+function ExplanationEventHandler(collapse) {
+    var _a;
+    if (collapse == true) {
+        var question = (_a = this.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement;
+        var answers = question.getElementsByTagName("answer");
+        //change icons and collapse
+        if (this.getAttribute("clicked") == "true") {
+            this.setAttribute("src", "ressources/icons/plus-solid.svg");
+            this.setAttribute("clicked", "false");
+            for (var i = answers.length - 1; i >= 0; i--) {
+                var answer = answers[i];
+                answer.getElementsByTagName("explanation")[0].setAttribute("visible", "false");
+            }
+        }
+        else {
+            this.setAttribute("src", "ressources/icons/minus-solid.svg");
+            this.setAttribute("clicked", "true");
+            for (var i = answers.length - 1; i >= 0; i--) {
+                var answer = answers[i];
+                answer.getElementsByTagName("explanation")[0].setAttribute("visible", "true");
+            }
+        }
+    }
+    else {
+        showExplanation(this);
+    }
+}
 // show <explanation>
-function showExplanation() {
-    var explanation = this.getElementsByTagName("explanation")[0];
+//Answer->DOM Manipulation
+function showExplanation(answer) {
+    var explanation = answer.getElementsByTagName("explanation")[0];
     if (explanation.getAttribute("visible") == "true") {
         explanation.setAttribute("visible", "false");
     }
     else {
         explanation.setAttribute("visible", "true");
+    }
+}
+// check click for correcct answer
+// depending on question type show either
+// - for multiplechoice just clicked answer
+// - for singlechoice all answers
+function checkAnswer() {
+    var _a, _b;
+    var question_type = (_a = this.parentElement) === null || _a === void 0 ? void 0 : _a.getAttribute("type");
+    if (question_type == "multiplechoice") {
+        showAnswer(this);
+    }
+    else if (question_type == "singlechoice") {
+        var answers = (_b = this.parentElement) === null || _b === void 0 ? void 0 : _b.getElementsByTagName("answer");
+        for (var i = (answers === null || answers === void 0 ? void 0 : answers.length) - 1; i >= 0; i--) {
+            var answer = answers[i];
+            showAnswer(answer);
+        }
+    }
+}
+// showAnswer
+// show icons and highlight correct answer
+function showAnswer(answer) {
+    answer.setAttribute("clicked", "true");
+    var img = answer.getElementsByTagName("img")[0];
+    if (answer.getAttribute("correct") == "true") {
+        img.setAttribute("src", "ressources/icons/check-solid.svg");
+    }
+    else {
+        img.setAttribute("src", "ressources/icons/xmark-solid.svg");
     }
 }
--- src/questionnaire.ts
+++ src/questionnaire.ts
@@ -10,32 +10,136 @@
 
 
 const q_col: HTMLCollection = document.getElementsByTagName("questionnaire") as HTMLCollection;
-// render every questionnaire
+// render every questionnaire in the HTML Document
 for (let i = q_col.length - 1; i >= 0; i--) {
   let questionnaire: HTMLElement = q_col[i] as HTMLElement;
   renderQuestionaire(questionnaire);
 }
-// render questionnaire
+
+// render questionnaire:
+// addEventListener for "click"-Events
+// build wrapper-<div> and <img>-icons for
+// - answer
+// - question
+
 function renderQuestionaire(questionnaire: HTMLElement) {
   console.log(questionnaire);
-  let a_col: HTMLCollection = questionnaire.getElementsByTagName("answer");
-  //addEventListener for every <answer>
-  for (let i = a_col.length - 1; i >= 0; i--) {
-    let answer: HTMLElement = a_col[i] as HTMLElement;
-    answer.addEventListener("click", showExplanation);
+  renderQuestions(questionnaire);
+  renderAnswers(questionnaire);
+}
+
+// renderQuestions
+// for every question:
+// add <div>-wrapper + <img>-icon (done)
+// add EventListener for CollapseAll-Function
+function renderQuestions(questionnaire: HTMLElement) {
+  let questions: HTMLCollection = questionnaire.getElementsByTagName("question");
+  for (let i = questions.length - 1; i >= 0; i--) {
+    let question: HTMLElement = questions[i] as HTMLElement;
+    let text = question.getElementsByTagName("p")[0] as HTMLParagraphElement;
+    // build div-wrapper
+    let new_div: HTMLDivElement = document.createElement("div");
+    new_div.setAttribute("class", "wrapper-question");
+    question.prepend(new_div);
+    // append text and img
+    let img = document.createElement("img");
+    img.setAttribute("src", "ressources/icons/plus-solid.svg");
+    new_div.append(text, img);
+    img.addEventListener("click", ExplanationEventHandler.bind(img, true));
   }
 }
-// get answers
 
 
+// questionnaire->DOM Manipulation
+function renderAnswers(questionnaire: HTMLElement) {
+  let answers: HTMLCollection = questionnaire.getElementsByTagName("answer");
+  //for every answer:
+  // add <div> wrapper + <img>-icon (done)
+  // add EventListener for AnswerClickEvents (done)
+  for (let i = answers.length - 1; i >= 0; i--) {
+    let answer: HTMLElement = answers[i] as HTMLElement;
+    // build div-wrapper
+    let new_div: HTMLDivElement = document.createElement("div");
+    let text = answer.getElementsByTagName("p")[0] as HTMLParagraphElement;
+    new_div.setAttribute("class", "wrapper-answer");
+    answer.prepend(new_div);
+    //append text and img
+    let img = document.createElement("img");
+    img.setAttribute("src", "ressources/icons/circle-regular.svg");
+    new_div.append(img, text);
+    answer.addEventListener("click", checkAnswer);
+    answer.addEventListener("click", ExplanationEventHandler.bind(answer, false));
+  }
+}
+// ExplanationEventHandler
+// Handles Events for shoowing explanation text
+function ExplanationEventHandler(this: HTMLElement, collapse: boolean) {
+  if (collapse == true) {
+    let question: HTMLElement = this.parentElement ?.parentElement as HTMLElement;
+    let answers: HTMLCollection = question.getElementsByTagName("answer") as HTMLCollection;
+    //change icons and collapse
+    if (this.getAttribute("clicked") == "true") {
+      this.setAttribute("src", "ressources/icons/plus-solid.svg");
+      this.setAttribute("clicked", "false");
+      for (let i = answers.length - 1; i >= 0; i--) {
+        let answer = answers[i] as HTMLElement;
+        answer.getElementsByTagName("explanation")[0].setAttribute("visible", "false");
+      }
+    }
+    else {
+      this.setAttribute("src", "ressources/icons/minus-solid.svg");
+      this.setAttribute("clicked", "true");
+      for (let i = answers.length - 1; i >= 0; i--) {
+        let answer = answers[i] as HTMLElement;
+        answer.getElementsByTagName("explanation")[0].setAttribute("visible", "true");
+      }
+    }
+  }
+  else {
+    showExplanation(this);
+  }
+}
 // show <explanation>
-function showExplanation(this: HTMLElement) {
-  let explanation: HTMLElement = this.getElementsByTagName("explanation")[0] as HTMLElement;
+//Answer->DOM Manipulation
+function showExplanation(answer: HTMLElement) {
+  let explanation: HTMLElement = answer.getElementsByTagName("explanation")[0] as HTMLElement;
   if (explanation.getAttribute("visible") == "true") {
     explanation.setAttribute("visible", "false");
   }
   else {
     explanation.setAttribute("visible", "true");
+  }
+}
 
+// check click for correcct answer
+// depending on question type show either
+// - for multiplechoice just clicked answer
+// - for singlechoice all answers
+function checkAnswer(this: HTMLElement) {
+  let question_type = this.parentElement ?.getAttribute("type");
+  if (question_type == "multiplechoice") {
+    showAnswer(this);
+  }
+  else if (question_type == "singlechoice") {
+    let answers = this.parentElement ?.getElementsByTagName("answer") as HTMLCollection;
+    for (let i = answers ?.length - 1; i >= 0; i--) {
+      let answer: HTMLElement = answers[i] as HTMLElement;
+      showAnswer(answer);
+
+    }
+  }
+}
+
+// showAnswer
+// show icons and highlight correct answer
+
+function showAnswer(answer: HTMLElement) {
+  answer.setAttribute("clicked", "true");
+  let img = answer.getElementsByTagName("img")[0];
+  if (answer.getAttribute("correct") == "true") {
+    img.setAttribute("src", "ressources/icons/check-solid.svg");
+  }
+  else {
+    img.setAttribute("src", "ressources/icons/xmark-solid.svg");
   }
 }

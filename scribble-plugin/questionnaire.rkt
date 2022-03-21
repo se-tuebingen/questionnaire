b90e7fd95460b133a9a09ffd42e73d36caf48e0a
Florian Kellner <mr.florian.kellner@web.de>
2022-03-21 11:39:17 +0100

    success: adding custom HTML

--- scribble-plugin/questionnaire.rkt
+++ scribble-plugin/questionnaire.rkt
@@ -1,8 +1,23 @@
 #lang racket/base
 
 (require scriblib/render-cond)
+(require scribble/core
+         scribble/html-properties
+         (only-in xml cdata))
 
 (provide step)
 
+;; custom HTML
+(define (question-tag-wrapper type)
+        (xexpr-property
+          (cdata #f #f "<question>")
+          (cdata #f #f "</question")))
+
+(define (question-tag type content)
+        (element (style #f (list (question-tag-wrapper type)))
+                 content))
+
 (define (step)
-  (cond-element [html "→"] [latex "$\\longrightarrow$"]))
+  (cond-element
+    [html (question-tag "singlechoice" "arrow")]
+    [latex "$\\longrightarrow$"]))

d7784786d8ccc81438ea2edb66f5023fadbe6a39
Florian Kellner <mr.florian.kellner@web.de>
2022-03-21 12:20:18 +0100

    WIP: more tags

--- scribble-plugin/questionnaire.rkt
+++ scribble-plugin/questionnaire.rkt
@@ -8,10 +8,11 @@
 
 (provide step)
 
-;; custom HTML
+;;;;;;;;;;; custom HTML tags
 
 (define questiontypes (or/c "singlechoice" "multiplechoice")) ; one-of does not work with strings
 
+;; question
 (define/contract
   (question-tag-wrapper type)
   (-> questiontypes xexpr-property?)
@@ -19,13 +20,46 @@
     (cdata #f #f (string-append
       "<question type=\"" type "\">"
       ))
-    (cdata #f #f "</question")))
+    (cdata #f #f "</question>")))
 
 (define/contract
   (question-tag type content)
   (-> questiontypes content? content?)
         (element (style #f (list (question-tag-wrapper type)))
                  content))
+
+;; answer
+(define/contract
+  (answer-tag-wrapper correct)
+  (-> boolean? xexpr-property?)
+  (xexpr-property
+    (cdata #f #f (string-append
+      "<answer correct=\"" (if correct "true" "false") "\">"
+      ))
+    (cdata #f #f "</answer>")))
+
+(define/contract
+  (answer-tag correct content)
+  (-> boolean? content? content?)
+        (element (style #f (list (answer-tag-wrapper correct)))
+                 content))
+
+;; explanation
+(define
+  explanation-tag-wrapper
+  (xexpr-property
+    (cdata #f #f "<explanation>")
+    (cdata #f #f "</explanation>")))
+
+(define/contract
+  (explanation-tag content)
+  (-> content? content?)
+      (element (style #f (list explanation-tag-wrapper))
+                content))
+
+;;;;;;;;;;;; Exposed Macros
+; single question
+; TODO
 
 (define (step)
   (cond-element

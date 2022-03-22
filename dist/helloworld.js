5d8f2bebbf680c582f25901c46123e4bf65afc0d
Florian Kellner <mr.florian.kellner@web.de>
2022-03-22 15:26:37 +0100

    remove hello world code and rename test document

--- dist/helloworld.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var testel = document.getElementById("test");
-if (testel) {
-    testel.innerHTML = "Hello from TypeScript!";
-}
-else {
-    window.alert("Element with id 'test' is missing from HTML!");
-}

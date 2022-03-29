99f2a0fa0e878b3a0be9feaa64202dc1f1b8f50d
Florian auf Tulpenlaptop <mr.florian.kellner@posteo.de>
2022-03-29 12:30:09 +0200

    basic API, very ugly

--- scribble-plugin/questionnaire.rkt
+++ scribble-plugin/questionnaire.rkt
@@ -8,162 +8,52 @@
 (require scribble/base)
 (require scribble/decode)
 
-(provide questionnaire question answer explanation setup-questionnaire)
+(provide questionnaire question answer setup-questionnaire)
 
-;;;;;;;;;;; custom HTML tags
+;;;;;;;;;;; Type Definitions
+(define questiontypes (or/c "singlechoice" "multiplechoice"))
+ ; one-of does not work with strings
 
-(define questiontypes (or/c "singlechoice" "multiplechoice")) ; one-of does not work with strings
+(struct answer-container (correct text explanation))
+;                         boolean content content
 
-;; questionnaire
-(define
-  questionnaire-tag-wrapper
-  (xexpr-property
-    (cdata #f #f "<questionnaire>")
-    (cdata #f #f "</questionnaire>")))
+(struct question-container (type text answers))
+;                 questiontypes content (listof answer-container)
 
-(define ;/contract
-  (questionnaire-tag content)
-  ;(-> content? content?)
-      (element (style #f (list questionnaire-tag-wrapper))
-                content))
+;;;;;;;;;;; HTML Part
+(define/contract
+  (render-html questions)
+  (-> (listof question-container?) any)
+  ""
+)
 
-;; question
-(define ;/contract
-  (question-tag-wrapper type)
-  ;(-> questiontypes xexpr-property?)
-  (xexpr-property
-    (cdata #f #f (string-append
-      "<question type=\"" type "\">"
-      ))
-    (cdata #f #f "</question>")))
+;;;;;;;;;;; Latex Part
+(define/contract
+  (render-latex questions)
+  (-> (listof question-container?) any)
+  ""
+)
 
-(define ;/contract
-  (question-tag type content)
-  ;(-> questiontypes content? content?)
-        (element (style #f (list (question-tag-wrapper type)))
-                 content))
 
-;; answer
-(define ;/contract
-  (answer-tag-wrapper correct)
-  ;(-> boolean? xexpr-property?)
-  (xexpr-property
-    (cdata #f #f (string-append
-      "<answer correct=\"" (if correct "true" "false") "\">"
-      ))
-    (cdata #f #f "</answer>")))
-
-(define ;/contract
-  (answer-tag correct content)
-  ;(-> boolean? content? content?)
-        (element (style #f (list (answer-tag-wrapper correct)))
-                 content))
-
-;; explanation
-(define
-  explanation-tag-wrapper
-  (xexpr-property
-    (cdata #f #f "<explanation>")
-    (cdata #f #f "</explanation>")))
+;;;;;;;;;;; Exposed API
+(define/contract
+  (answer correct text explanation)
+  (-> boolean? content? content? answer-container?)
+  (answer-container correct text explanation)
+)
 
 (define/contract
-  (explanation-tag content)
-  (-> content? content?)
-      (element (style #f (list explanation-tag-wrapper))
-                content))
+  (question type text answers)
+  (-> questiontypes content? (listof answer-container?) question-container?)
+  (question-container type text answers)
+)
 
-;;;;; Latex versions
-(define answer-type (cons/c boolean? (cons/c content? content?)))
-(define question-type (cons/c content? (listof answer-type)))
-(define questionnaire-type (listof question-type))
-
-; latex-renderer for questions
-(define
-  (latex-answer content)
-  (item (car (cdr content))))
-(define
-  (latex-question content)
-  (list (centered (car content))
-        (itemlist (map latex-answer (cdr content)))))
-(define
-  (latex-questions content)
-  (map latex-question content))
-
-; latex-renderer for solutions
-(define
-  (latex-solutions content) "")
-
-;;; pass content up in order to split it into questions and solutions
-; explanation
 (define/contract
-  (explanation-latex content)
-  (-> content? content?)
-  "")
-  ; content)
-
-; answer
-(define (is-no-whitespace? x)
-  (not (or (string=? "\n" x) (string=? "" x))))
-(define ;/contract
-  (answer-latex correct content)
-  (item content))
-  ; ; (-> boolean? (listof content?))
-  ; (filter is-no-whitespace? content))
-
-; question
-(define
-  (question-latex content)
-  (list (centered (car content)) (cdr content)))
-  ; (filter is-no-whitespace? content))
-
-; questionnaire
-(define
-  (questionnaire-latex content)
-  content)
-  ; (decode (list
-  ;   (subsection "Questionnaire")
-  ;   (decode content)
-  ;     ; (latex-solutions content)))
-  ; )))
-
-
-
-
-
-;;;;;;;;;;;; Exposed Macros
-
-; questionnaire
-(define
-  (questionnaire . questions)
+  (questionnaire questions)
+  (-> (listof question-container?) any)
   (cond-element
-    [html (questionnaire-tag questions)]
-    [latex (questionnaire-latex questions)]
-  )
-)
-
-; single question
-(define
-  (question type . content)
-  (cond-element
-    [html (question-tag type content)]
-    [latex (question-latex content)] ; todo latex implement. of mc questions
-  )
-)
-
-; answer
-(define
-  (answer correct . content)
-  (cond-element
-    [html (answer-tag correct content)]
-    [latex (answer-latex correct content)]
-  )
-)
-; explanation
-(define
-  (explanation . exp)
-  (cond-element
-    [html (explanation-tag exp)]
-    [latex (explanation-latex exp)]
+    [html (render-html questions)]
+    [latex (render-latex questions)]
   )
 )
 
@@ -175,3 +65,157 @@
              (attributes `((type . "text/javascript")
                            (src . "questionnaire.js" )))))
    '()))
+
+
+;; questionnaire
+; (define
+;   questionnaire-tag-wrapper
+;   (xexpr-property
+;     (cdata #f #f "<questionnaire>")
+;     (cdata #f #f "</questionnaire>")))
+;
+; (define ;/contract
+;   (questionnaire-tag content)
+;   ;(-> content? content?)
+;       (element (style #f (list questionnaire-tag-wrapper))
+;                 content))
+;
+; ;; question
+; (define ;/contract
+;   (question-tag-wrapper type)
+;   ;(-> questiontypes xexpr-property?)
+;   (xexpr-property
+;     (cdata #f #f (string-append
+;       "<question type=\"" type "\">"
+;       ))
+;     (cdata #f #f "</question>")))
+;
+; (define ;/contract
+;   (question-tag type content)
+;   ;(-> questiontypes content? content?)
+;         (element (style #f (list (question-tag-wrapper type)))
+;                  content))
+;
+; ;; answer
+; (define ;/contract
+;   (answer-tag-wrapper correct)
+;   ;(-> boolean? xexpr-property?)
+;   (xexpr-property
+;     (cdata #f #f (string-append
+;       "<answer correct=\"" (if correct "true" "false") "\">"
+;       ))
+;     (cdata #f #f "</answer>")))
+;
+; (define ;/contract
+;   (answer-tag correct content)
+;   ;(-> boolean? content? content?)
+;         (element (style #f (list (answer-tag-wrapper correct)))
+;                  content))
+;
+; ;; explanation
+; (define
+;   explanation-tag-wrapper
+;   (xexpr-property
+;     (cdata #f #f "<explanation>")
+;     (cdata #f #f "</explanation>")))
+;
+; (define/contract
+;   (explanation-tag content)
+;   (-> content? content?)
+;       (element (style #f (list explanation-tag-wrapper))
+;                 content))
+
+; ;;;;; Latex versions
+; (define answer-type (cons/c boolean? (cons/c content? content?)))
+; (define question-type (cons/c content? (listof answer-type)))
+; (define questionnaire-type (listof question-type))
+;
+; ; latex-renderer for questions
+; (define
+;   (latex-answer content)
+;   (item (car (cdr content))))
+; (define
+;   (latex-question content)
+;   (list (centered (car content))
+;         (itemlist (map latex-answer (cdr content)))))
+; (define
+;   (latex-questions content)
+;   (map latex-question content))
+;
+; ; latex-renderer for solutions
+; (define
+;   (latex-solutions content) "")
+;
+; ;;; pass content up in order to split it into questions and solutions
+; ; explanation
+; (define/contract
+;   (explanation-latex content)
+;   (-> content? content?)
+;   "")
+;   ; content)
+;
+; ; answer
+; (define (is-no-whitespace? x)
+;   (not (or (string=? "\n" x) (string=? "" x))))
+; (define ;/contract
+;   (answer-latex correct content)
+;   (item content))
+;   ; ; (-> boolean? (listof content?))
+;   ; (filter is-no-whitespace? content))
+;
+; ; question
+; (define
+;   (question-latex content)
+;   (list (centered (car content)) (cdr content)))
+;   ; (filter is-no-whitespace? content))
+;
+; ; questionnaire
+; (define
+;   (questionnaire-latex content)
+;   content)
+;   ; (decode (list
+;   ;   (subsection "Questionnaire")
+;   ;   (decode content)
+;   ;     ; (latex-solutions content)))
+;   ; )))
+;
+;
+;
+;
+;
+; ;;;;;;;;;;;; Exposed Macros
+;
+; ; questionnaire
+; (define
+;   (questionnaire . questions)
+;   (cond-element
+;     [html (questionnaire-tag questions)]
+;     [latex (questionnaire-latex questions)]
+;   )
+; )
+;
+; ; single question
+; (define
+;   (question type . content)
+;   (cond-element
+;     [html (question-tag type content)]
+;     [latex (question-latex content)] ; todo latex implement. of mc questions
+;   )
+; )
+;
+; ; answer
+; (define
+;   (answer correct . content)
+;   (cond-element
+;     [html (answer-tag correct content)]
+;     [latex (answer-latex correct content)]
+;   )
+; )
+; ; explanation
+; (define
+;   (explanation . exp)
+;   (cond-element
+;     [html (explanation-tag exp)]
+;     [latex (explanation-latex exp)]
+;   )
+; )
--- scribble-plugin/test.scrbl
+++ scribble-plugin/test.scrbl
@@ -16,129 +16,103 @@ Questionnaire-Modul testen.
 Test einer Single-Choice-Frage:
 
 @questionnaire[
-  @question["singlechoice"]{
-    What is this course about?
-    @answer[#f]{
-      Learning to swim
-      @explanation{We do not have a pool}
-    }
-    @answer[#f]{
-      Learning to sing.
-      @explanation{Singing is hard with masks on or network latency.}
-    }
-    @answer[#t]{
-      Learning to program
-      @explanation{Correct, you are reading this on some computer.}
-    }
-  }
+  (list
+  @question[
+    "singlechoice"
+    "What is this course about?"
+    (list
+        @answer[
+          #f
+          "Learning to swim"
+          "We do not have a pool"
+        ]
+        @answer[
+          #f
+          "Learning to sing"
+          "Singing is hard with masks on or network latency"
+        ]
+        @answer[
+          #t
+          "Learning to program"
+          "You are sitting at a computer, aren't you?"
+        ]
+    )
+  ])
 ]
 @setup-questionnaire
 
-@section{Multiple-Choice}
-
-Test einer Multiple-Choice-Frage:
-
-@questionnaire[
-  @question["multiplechoice"]{
-    What does TS stand for?
-    @answer[#t]{
-      TeamSpeak
-      @explanation{etc}
-    }
-    @answer[#f]{
-      Torus Examination
-      @explanation{although technically...}
-    }
-    @answer[#t]{
-      TypeScript
-      @explanation{Yup}
-    }
-  }
-]
-@setup-questionnaire
-
-@section{Block mit mehreren Fragen}
-
-Test eines Blocks mit mehreren Fragen
-
-@questionnaire[
-  @question["multiplechoice"]{
-    What does TS stand for?
-    @answer[#t]{
-      TeamSpeak
-      @explanation{etc}
-    }
-    @answer[#f]{
-      Torus Examination
-      @explanation{although technically...}
-    }
-    @answer[#t]{
-      TypeScript
-      @explanation{Yup}
-    }
-  }
-  @question["singlechoice"]{
-    What is this course about?
-    @answer[#f]{
-      Learning to swim
-      @explanation{We do not have a pool}
-    }
-    @answer[#f]{
-      Learning to sing.
-      @explanation{Singing is hard with masks on or network latency.}
-    }
-    @answer[#t]{
-      Learning to program
-      @explanation{Correct, you are reading this on some computer.}
-    }
-  }
-  @question["singlechoice"]{
-    Why is this happening?
-    @answer[#f]{
-      Yes
-      @explanation{But actually no}
-    }
-    @answer[#f]{
-      No
-      @explanation{But actually yes}
-    }
-    @answer[#t]{
-      Goat
-      @explanation{Whatever floats your goat}
-    }
-  }
-]
-@setup-questionnaire
-
-@section{Sonderzeichen und Rich Text}
-
-Test, ob Bilder, Code und Auflistungen als Frageinhalte richtig dargestellt werden:
-
-@questionnaire[
-  @question["multiplechoice"]{
-    Choose all answers that
-    @itemlist[@item{Look right}
-              @item{Are Correct}]
-    @answer[#t]{
-      @centered{Centered Text}
-      @explanation{@centered{@bold{bold choice}}}
-    }
-    @answer[#t]{
-      @(colorize (filled-ellipse 40 40) "beige")
-      @explanation{@(colorize (filled-ellipse 40 40) "brown")}
-    }
-    @answer[#f]{
-      @tabular[#:sep @hspace[1]
-         (list (list "soup" "gazpacho")
-               (list "soup" "tonjiru"))]
-      @explanation{@tabular[#:style 'boxed
-         #:column-properties '(left right)
-         #:row-properties '(bottom-border ())
-         (list (list @bold{recipe}   @bold{vegetable})
-               (list "caldo verde"   "kale")
-               (list "kinpira gobō"  "burdock")
-               (list "makizushi"     'cont))]}
-    }
-  }
-]
-@setup-questionnaire
+@; @section{Multiple-Choice}
+@;
+@; Test einer Multiple-Choice-Frage:
+@;
+@; @questionnaire[
+@;   @question["multiplechoice"]{
+@;     What does TS stand for?
+@;     @answer[#t]{
+@;       TeamSpeak
+@;       @explanation{etc}
+@;     }
+@;     @answer[#f]{
+@;       Torus Examination
+@;       @explanation{although technically...}
+@;     }
+@;     @answer[#t]{
+@;       TypeScript
+@;       @explanation{Yup}
+@;     }
+@;   }
+@; ]
+@; @setup-questionnaire
+@;
+@; @section{Block mit mehreren Fragen}
+@;
+@; Test eines Blocks mit mehreren Fragen
+@;
+@; @questionnaire[
+@;   @question["multiplechoice"]{
+@;     What does TS stand for?
+@;     @answer[#t]{
+@;       TeamSpeak
+@;       @explanation{etc}
+@;     }
+@;     @answer[#f]{
+@;       Torus Examination
+@;       @explanation{although technically...}
+@;     }
+@;     @answer[#t]{
+@;       TypeScript
+@;       @explanation{Yup}
+@;     }
+@;   }
+@;   @question["singlechoice"]{
+@;     What is this course about?
+@;     @answer[#f]{
+@;       Learning to swim
+@;       @explanation{We do not have a pool}
+@;     }
+@;     @answer[#f]{
+@;       Learning to sing.
+@;       @explanation{Singing is hard with masks on or network latency.}
+@;     }
+@;     @answer[#t]{
+@;       Learning to program
+@;       @explanation{Correct, you are reading this on some computer.}
+@;     }
+@;   }
+@;   @question["singlechoice"]{
+@;     Why is this happening?
+@;     @answer[#f]{
+@;       Yes
+@;       @explanation{But actually no}
+@;     }
+@;     @answer[#f]{
+@;       No
+@;       @explanation{But actually yes}
+@;     }
+@;     @answer[#t]{
+@;       Goat
+@;       @explanation{Whatever floats your goat}
+@;     }
+@;   }
+@; ]
+@; @setup-questionnaire

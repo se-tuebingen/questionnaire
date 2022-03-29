658d61541040e64b9817759d753e610ad9d96711
Linus S <66415456+linususer@users.noreply.github.com>
2022-03-29 21:31:25 +0200

    changed test.sh and added output folder for html and pdf outputs

--- scribble-plugin/test.sh
+++ scribble-plugin/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "copying latest version of JS plugin to source folder"
-cp ../dist/questionnaire.js .
+cp dist/questionnaire.js .
 
 echo "generating HTML output"
-scribble --htmls --dest html test.scrbl
+raco scribble --htmls --dest output/html scribble-plugin/test.scrbl
 
 echo "producing pdf output"
-scribble --pdf --dest pdf test.scrbl
+raco scribble --pdf --dest output/pdf scribble-plugin/test.scrbl

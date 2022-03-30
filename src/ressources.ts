c97969499b4f656789f1ed0917a190c971b2d99d
Florian auf Tulpenlaptop <mr.florian.kellner@posteo.de>
2022-03-30 16:34:39 +0200

    properly namespace styles

--- src/ressources.ts
+++ src/ressources.ts
@@ -6,19 +6,12 @@ export const circle_regular = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDo
 export const minus_solid = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0NDggNTEyIj48IS0tISBGb250IEF3ZXNvbWUgUHJvIDYuMS4wIGJ5IEBmb250YXdlc29tZSAtIGh0dHBzOi8vZm9udGF3ZXNvbWUuY29tIExpY2Vuc2UgLSBodHRwczovL2ZvbnRhd2Vzb21lLmNvbS9saWNlbnNlIChDb21tZXJjaWFsIExpY2Vuc2UpIENvcHlyaWdodCAyMDIyIEZvbnRpY29ucywgSW5jLiAtLT48cGF0aCBkPSJNNDAwIDI4OGgtMzUyYy0xNy42OSAwLTMyLTE0LjMyLTMyLTMyLjAxczE0LjMxLTMxLjk5IDMyLTMxLjk5aDM1MmMxNy42OSAwIDMyIDE0LjMgMzIgMzEuOTlTNDE3LjcgMjg4IDQwMCAyODh6Ii8+PC9zdmc+`;
 export const plus_solid = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0NDggNTEyIj48IS0tISBGb250IEF3ZXNvbWUgUHJvIDYuMS4wIGJ5IEBmb250YXdlc29tZSAtIGh0dHBzOi8vZm9udGF3ZXNvbWUuY29tIExpY2Vuc2UgLSBodHRwczovL2ZvbnRhd2Vzb21lLmNvbS9saWNlbnNlIChDb21tZXJjaWFsIExpY2Vuc2UpIENvcHlyaWdodCAyMDIyIEZvbnRpY29ucywgSW5jLiAtLT48cGF0aCBkPSJNNDMyIDI1NmMwIDE3LjY5LTE0LjMzIDMyLjAxLTMyIDMyLjAxSDI1NnYxNDRjMCAxNy42OS0xNC4zMyAzMS45OS0zMiAzMS45OXMtMzItMTQuMy0zMi0zMS45OXYtMTQ0SDQ4Yy0xNy42NyAwLTMyLTE0LjMyLTMyLTMyLjAxczE0LjMzLTMxLjk5IDMyLTMxLjk5SDE5MnYtMTQ0YzAtMTcuNjkgMTQuMzMtMzIuMDEgMzItMzIuMDFzMzIgMTQuMzIgMzIgMzIuMDF2MTQ0aDE0NEM0MTcuNyAyMjQgNDMyIDIzOC4zIDQzMiAyNTZ6Ii8+PC9zdmc+`;
 export const xmark_solid = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMjAgNTEyIj48IS0tISBGb250IEF3ZXNvbWUgUHJvIDYuMS4wIGJ5IEBmb250YXdlc29tZSAtIGh0dHBzOi8vZm9udGF3ZXNvbWUuY29tIExpY2Vuc2UgLSBodHRwczovL2ZvbnRhd2Vzb21lLmNvbS9saWNlbnNlIChDb21tZXJjaWFsIExpY2Vuc2UpIENvcHlyaWdodCAyMDIyIEZvbnRpY29ucywgSW5jLiAtLT48cGF0aCBkPSJNMzEwLjYgMzYxLjRjMTIuNSAxMi41IDEyLjUgMzIuNzUgMCA0NS4yNUMzMDQuNCA0MTIuOSAyOTYuMiA0MTYgMjg4IDQxNnMtMTYuMzgtMy4xMjUtMjIuNjItOS4zNzVMMTYwIDMwMS4zTDU0LjYzIDQwNi42QzQ4LjM4IDQxMi45IDQwLjE5IDQxNiAzMiA0MTZTMTUuNjMgNDEyLjkgOS4zNzUgNDA2LjZjLTEyLjUtMTIuNS0xMi41LTMyLjc1IDAtNDUuMjVsMTA1LjQtMTA1LjRMOS4zNzUgMTUwLjZjLTEyLjUtMTIuNS0xMi41LTMyLjc1IDAtNDUuMjVzMzIuNzUtMTIuNSA0NS4yNSAwTDE2MCAyMTAuOGwxMDUuNC0xMDUuNGMxMi41LTEyLjUgMzIuNzUtMTIuNSA0NS4yNSAwczEyLjUgMzIuNzUgMCA0NS4yNWwtMTA1LjQgMTA1LjRMMzEwLjYgMzYxLjR6Ii8+PC9zdmc+`;
-export const style = `body {
-  width: 100%;
-  margin: 0;
-  padding: 0;
-  font-family: monospace;
+export const style = `questionnaire {
+  display: block;
+  margin: 40px 0 100px ;
 }
 
-questionnaire{
-  display:block;
-  margin:40px 0 100px ;
-}
-
-.content-wrapper {
+questionnaire .content-wrapper {
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
@@ -26,14 +19,14 @@ questionnaire{
   padding: 10px;
   justify-content: center;
 }
-question{
+questionnaire question{
   display:none;
 }
-.question-overview{
+questionnaire .question-overview{
 margin: 0 auto 10px;
 font-size:1.1em;
 }
-question{
+questionnaire question{
   width: 90%;
   margin: 15px auto 15px;
   font-size: 18pt;
@@ -41,24 +34,24 @@ question{
   background-color: #fcfcfc;
 }
 
-.question-header, .question-footer, .wrapper-answer {
+questionnaire .question-header, questionnaire .question-footer, questionnaire .wrapper-answer {
   display: inline-flex;
   width: 100%;
 }
 
-.question-header {
+questionnaire .question-header {
   justify-content: space-between;
 }
 
-.question-footer{
+questionnaire .question-footer{
   justify-content: center;
 }
 
-[visible=true]{
+questionnaire [visible=true]{
     display:block;
 }
 
-.wrapper-answer, answer [visible=true] {
+questionnaire .wrapper-answer, questionnaire answer [visible=true] {
   border: 1px solid #eee;
   padding: 5px 12px;
   font-size: 14pt;
@@ -66,7 +59,7 @@ question{
   width:90%;
 }
 
-answer p {
+questionnaire answer p {
   margin: 0 0 0 16px;
   padding: 6px;
   /*font-size: 12pt;
@@ -74,22 +67,22 @@ answer p {
   width:100%;*/
 }
 
-.wrapper-answer:hover, img:hover, .change-question-button:hover {
+questionnaire .wrapper-answer:hover, questionnaire img:hover, questionnaire .change-question-button:hover {
   cursor: pointer;
   /*background-color: #ddd;*/
 }
 
-.wrapper-answer:hover {
+questionnaire .wrapper-answer:hover {
   background-color: #eee;
 }
 
 
-explanation {
+questionnaire explanation {
   display: none;
   /*max-width: 30vw;*/
 }
 
-answer [visible=true] {
+questionnaire answer [visible=true] {
   margin: 5px 0 20px;
   padding: 15px 12px;
   font-size: 12pt;
@@ -98,36 +91,36 @@ answer [visible=true] {
   background-color: #fdfdfd;
 }
 
-answer [visible=true] p {
+questionnaire answer [visible=true] p {
   border: 0;
 }
 
-img {
+questionnaire img {
   height: auto;
   width: 20px;
 }
 
-[clicked=true] .wrapper-answer, [clicked=true] .wrapper-answer:hover{
+questionnaire [clicked=true] .wrapper-answer, questionnaire [clicked=true] .wrapper-answer:hover{
   background-color:#d30000;
 }
 
-[clicked=true][correct=true] .wrapper-answer{
+questionnaire [clicked=true][correct=true] .wrapper-answer{
   background-color:#aceb84;
 }
 
-.change-question-button{
+questionnaire .change-question-button{
   padding:15px;
   margin:15px 15px 0;
   border: 4px solid #bbb;
   border-radius: 7px;
   font-size:1.3em;
 }
-.change-question-button:hover{
+questionnaire .change-question-button:hover{
   background-color: #bbb;
 }
 
 @media (min-width: 768px) {
-  question {
+  questionnaire question {
     max-width: 600px;
   }
 }`;

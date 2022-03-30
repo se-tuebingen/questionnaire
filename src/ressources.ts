7823a17cc1b57e69a84d454867f3803ebef0cc76
Linus Szillat <66415456+linususer@users.noreply.github.com>
2022-03-30 15:42:44 +0200

    tests

--- src/ressources.ts
+++ src/ressources.ts
@@ -130,5 +130,24 @@ img {
   question {
     max-width: 600px;
   }
+}
+.error-wrapper{
+  display:block;
+  max-width: 600px;
+  border: 5px solid red;
+   margin: 0 auto;
+  padding:0 20px;
+}
+.error-header{
+
+}
+.error-box{
+font-size:16pt;
+line-height:1.5em;
+}
+
+.error-message{
+  font-family:monospace;
+  font-size:12pt;
 }`;
 }

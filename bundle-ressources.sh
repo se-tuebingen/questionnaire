96a32282e9b45f09bbc0bb8ef19776c8b49583f2
Florian Kellner <mr.florian.kellner@web.de>
2022-04-19 16:30:09 +0200

    hopefully prevent lines from breaking on MacOS

--- bundle-ressources.sh
+++ bundle-ressources.sh
@@ -7,10 +7,19 @@
 # reset ressources.ts
 echo "module Ressources {" > src/ressources.ts
 
+# Base64 flag differs depending on OS
+# - Linux: --wrap=0
+# - MacOS: --break=0
+if [ $(uname) == Linux ]; then
+  base64flag="--wrap=0";
+else
+  base64flag="--break=0";
+fi
+
 # SVG icons
 cd src/ressources/icons
 for i in *; do
-  echo "export const $(basename $i .svg | sed s/-/_/g) = \`data:image/svg+xml;base64,$(base64 --wrap=0 $i)\`;" >> ../../ressources.ts;
+  echo "export const $(basename $i .svg | sed s/-/_/g) = \`data:image/svg+xml;base64,$(base64 $base64flag $i)\`;" >> ../../ressources.ts;
 done;
 
 # css style
